71c3880a4404638e44aaeed7635e2e3a180e722a
Zihary <zihary32lety@hotmail.com>
2023-10-14 15:41:58 -0700

    Procesamiento documentos JSON con GSON

--- src/main/java/org/example/Actividad_c.java
+++ src/main/java/org/example/Actividad_c.java
@@ -1,33 +1,50 @@
 package org.example;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Actividad_c {
     public static void main(String[] args) {
         try {
-
             File xmlFile = new File("car_sales.xml");
-            String xmlString = new String(java.nio.file.Files.readAllBytes(xmlFile.toPath()));
+            String xmlString = new String(Files.readAllBytes(Paths.get("car_sales.xml")));
 
             JSONObject jsonObject = XML.toJSONObject(xmlString).getJSONObject("car_sales");
+            JSONArray salesArray = jsonObject.getJSONArray("sale");
 
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String jsonString = gson.toJson(jsonObject);
+            JSONArray simplifiedArray = new JSONArray();
+            for (int i = 0; i < salesArray.length(); i++) {
+                JSONObject saleObject = salesArray.getJSONObject(i);
+                simplifiedArray.put(saleObject);
+            }
 
-            System.out.println(jsonString);
+            JSONObject finalObject = new JSONObject();
+            finalObject.put("sales", simplifiedArray);
 
             try (FileWriter fileWriter = new FileWriter("car_sales-Act-C.json")) {
-                fileWriter.write(jsonString);
+                fileWriter.write("[");
+                for (int i = 0; i < simplifiedArray.length(); i++) {
+                    if (i > 0) {
+                        fileWriter.write("," + System.lineSeparator());
+                    }
+                    fileWriter.write(simplifiedArray.getJSONObject(i).toString());
+                }
+                fileWriter.write("]");
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            String jsonContent = new String(Files.readAllBytes(Paths.get("car_sales-Act-C.json")));
+            System.out.println("Contenido del archivo JSON:");
+            System.out.println(jsonContent);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
